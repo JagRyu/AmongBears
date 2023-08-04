f5fe2b747cc84b5226a800918b10f0397851a36c
CuteRabbits <woejfl0824@naver.com>
2023-08-04 18:53:35 +0900

    조이스틱 작업중

---
+++ Assets/1. Scripts/Player/JoyStick.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyStick : MonoBehaviour
+{
+
+    bool isDrag;
+    // 1. 스틱 드래그 + 제한
+    // 2. 드래그한 만큼 캐릭터 이동
+    private void Update()
+    {
+        
+    }
+    // 스틱을 누르면 호출
+    public void ClickStick()
+    {
+
+    }
+
+}
