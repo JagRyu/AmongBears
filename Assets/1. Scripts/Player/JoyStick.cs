5b6efa5413dd0e102b13be2b271d529fbd487fab
CuteRabbits <woejfl0824@naver.com>
2023-08-04 23:18:21 +0900

    조이스틱 & 터치 화면 이동

--- Assets/1. Scripts/Player/JoyStick.cs
+++ Assets/1. Scripts/Player/JoyStick.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class JoyStick : MonoBehaviour
 {
-
+    //stick의 위치
+    public RectTransform stick, backGround;
+    PlayerCrt playerCtr;
     bool isDrag;
-    // 1. 스틱 드래그 + 제한
+    float limit;
+    // 1. 스틱 드래그 + 제한(움직임이 큰원 안에서 극한되고, 마우스를 놓았을 때 중앙에 위치하게)
     // 2. 드래그한 만큼 캐릭터 이동
+    private void Start()
+    {
+        playerCtr = GetComponent<PlayerCrt>();
+        limit = backGround.rect.width * 0.5f;
+    }
     private void Update()
     {
-        
+        // 드래그 하는 동안
+        if (isDrag)
+        {
+            Vector2 vec = Input.mousePosition - backGround.position;
+            // ClampMagnitude(,) => 어떤 값을 얼마만큼 제한해줄지
+            stick.localPosition = Vector2.ClampMagnitude(vec, limit);
+
+            Vector3 dir = (stick.position - backGround.position).normalized;
+            transform.position += dir * playerCtr.speed * Time.deltaTime;
+
+            // 조이스틱 클릭 떼어냈을 때(드래그 끝나면)
+            if (Input.GetMouseButtonUp(0))
+            {
+                stick.localPosition = new Vector3(0, 0, 0);
+                isDrag = false;
+            }
+        }
     }
     // 스틱을 누르면 호출
     public void ClickStick()
     {
-
+        isDrag = true;
     }
 
 }
--- Assets/1. Scripts/Player/PlayerCrt.cs
+++ Assets/1. Scripts/Player/PlayerCrt.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerCrt : MonoBehaviour
 {
+    public GameObject joyStick;
     public float speed;
-
+    public bool isJoyStick;
     private void Start()
     {
         Camera.main.transform.parent = transform;
@@ -19,12 +20,22 @@ public class PlayerCrt : MonoBehaviour
     //캐릭터 움직임 관리
     void Move()
     {
-        //클릭했는지 판단
-        if (Input.GetMouseButton(0))
+        if (isJoyStick)
         {
-            Vector3 dir = (Input.mousePosition - new Vector3(Screen.width * 0.5f, Screen.height * 0.5f)).normalized;
-
-            transform.position += dir * speed * Time.deltaTime;
+            joyStick.SetActive(true);
         }
+        else
+        {
+            joyStick.SetActive(false);
+
+            //클릭했는지 판단
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 dir = (Input.mousePosition - new Vector3(Screen.width * 0.5f, Screen.height * 0.5f)).normalized;
+
+                transform.position += dir * speed * Time.deltaTime;
+            }
+        }
+       
     }
 }
